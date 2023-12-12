c16694fc461edb75f0f22f6af39a9db64c5da38b
Ridwan Lawal <ridwan@Ridwans-MacBook-Pro.local>
2023-12-12 09:51:47 -0600

    connector-test

---
+++ .test-aws/credential.tf
@@ -0,0 +1,31 @@
+
+/*
+[default]
+aws_access_key_id = AKIA6EPEBOQVP6QBDGW4
+aws_secret_access_key = Oo7+FZ1ku1zu9s2/jGAyLUcrRyg2MPK/zD5r+9Us
+*/
+
+provider "aws" {
+  region = "us-east-2" # Set your desired AWS region
+  access_key = "AKIA6EPEBOQVP6QBDGW4"
+  secret_key = "Oo7+FZ1ku1zu9s2/jGAyLUcrRyg2MPK/zD5r+9Us"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "ridwan-repro-bucket"
+
+  tags = {
+    Name = "netsuite-splunk"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
+  
+}
