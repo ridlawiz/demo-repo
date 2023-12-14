5a37fc5a903790ed691d321f8c979bdc4f41c878
Ridwan Lawal <ridwan@Ridwans-MacBook-Pro.local>
2023-12-13 18:03:51 -0600

    port 22 inbound traffic added

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,10 +1,17 @@
 resource "aws_security_group" "example" {
   name        = "web_sg"
-  description = "Allow inbound traffic on ports 80"
+  description = "Allow inbound traffic on ports 80 and 22"
 
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  
   }
