967974ebae691e8aa4fa93b9d886e97fac37ad8c
ridlawiz <150713783+ridlawiz@users.noreply.github.com>
2024-08-28 14:58:09 -0500

    Update security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -23,3 +23,29 @@ resource "aws_security_group" "example" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "example" {
+  name        = "web_sg"
+  description = "Allow inbound traffic on ports 80 and 22"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
